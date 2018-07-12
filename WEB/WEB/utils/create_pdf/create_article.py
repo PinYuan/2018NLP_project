58fe4164eaf616b1b164efea73db257aae7dec06
ljie <ljy110040@gapp.nthu.edu.tw>
2018-07-12 19:41:32 +0800

    modify AJAX to apply all pos

--- WEB/WEB/utils/create_pdf/create_article.py
+++ WEB/WEB/utils/create_pdf/create_article.py
@@ -1,90 +1,25 @@
-from reportlab.lib.pagesizes import letter, A4
-from reportlab.lib.styles import ParagraphStyle
-from reportlab.lib.units import inch
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, KeepTogether
-
-from reportlab.lib.enums import TA_LEFT, TA_CENTER
-from reportlab.lib.colors import (
-    black,
-    purple,
-    white,
-    yellow
-)
-
 import re
 from collections import defaultdict, Counter
-from nltk.corpus import wordnet # To get words in dictionary with their parts of speech
-from nltk.stem import WordNetLemmatizer # lemmatizes word based on it's parts of speech
 from utils.senttag import parse_sent
-from pprint import pprint
 
+# build level word list
 data_pwd = './utils/data/levelWord/'
-A_word = set()
-B_word = set()
-C_word = set()
-
-def get_pos(word):
-    w_synsets = wordnet.synsets(word)
-
-    pos_counts = Counter()
-    pos_counts["n"] = len(  [ item for item in w_synsets if item.pos()=="n"]  )
-    pos_counts["v"] = len(  [ item for item in w_synsets if item.pos()=="v"]  )
-    pos_counts["a"] = len(  [ item for item in w_synsets if item.pos()=="a"]  )
-    pos_counts["r"] = len(  [ item for item in w_synsets if item.pos()=="r"]  )
+A_word = set(open(data_pwd + 'A_level_word.txt', 'r').readlines()[0].split())
+B_word = set(open(data_pwd + 'B_level_word.txt', 'r').readlines()[0].split())
+C_word = set(open(data_pwd + 'C_level_word.txt', 'r').readlines()[0].split())
     
-    most_common_pos_list = pos_counts.most_common(3)
-    # first indexer for getting the top POS from list, second indexer for getting POS from tuple( POS: count )
-    #print(most_common_pos_list[0])
-    if most_common_pos_list[0][1] == 0:
-        return None
-    return most_common_pos_list[0][0] 
-
-# Read statistics file
-#myDict = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: 0))) 
-
-def get_level_set():
-    f = open(data_pwd + 'A_level_word.txt', 'r')
-    words = f.readlines()[0].split()
-    for word in words:
-        A_word.add(word)
-    f.close()
-    
-    f = open(data_pwd + 'B_level_word.txt', 'r')
-    words = f.readlines()[0].split()
-    for word in words:
-        B_word.add(word)
-    f.close()
-    
-    f = open(data_pwd + 'C_level_word.txt', 'r')
-    words = f.readlines()[0].split()
-    for word in words:
-        C_word.add(word)
-    f.close()
-    
-def create_article(title, user_level, content, stylesheet, filename, verb, noun, adj):
-    get_level_set()
-    
-    doc = SimpleDocTemplate(filename, pagesize=letter)
-    parts = []
-    
-    spacer = KeepTogether(Spacer(0, 0.25*inch))
-    
-    parts.append(Paragraph(title, stylesheet['title']))
-    para_num = 1
+def create_article(title, user_level, content, filename, verb, noun, adj):
     new_content = []
-    word_list = set(['word', 'span', 'data', 'pos', 'u', 'datum'])
+    word_list = set(['word', 'span', 'data', 'pos', 'u', 'datum']) # prevent replace loop
     for c in content:
-        _type = c[0]
-        text = c[1]
+        _type, text = c[0], c[1]
         if _type == 'p':
             new_para = ''
-            _tag_para = ''
             for sent in text:
-                _sent = sent
                 parse = parse_sent(sent)
-                pos_parse = [y for x in parse[2] for y in x.split()]
                 words = [y for x in parse[0] for y in x.split()]
                 lemma_words = [y for x in parse[1] for y in x.split()]
+                pos_parse = [y for x in parse[2] for y in x.split()]
                 pos_num = 0
                 word_set = set()
                 for word in words:
@@ -95,8 +30,7 @@ def create_article(title, user_level, content, stylesheet, filename, verb, noun,
                         continue
                     word_set.add(lemma_word)
                     pos = pos_parse[pos_num]
-                    clean_word = re.sub('^[^a-zA-Z0-9]', '', word) # "word
-                    clean_word = re.sub('[^a-zA-Z0-9]$', '', clean_word) # word?
+                    clean_word = re.sub('[^a-zA-Z0-9]$', '', re.sub('^[^a-zA-Z0-9]', '', word)) # "word, word?
                     if not re.match('\w', clean_word):
                         pos_num += 1
                         continue # --
@@ -105,46 +39,22 @@ def create_article(title, user_level, content, stylesheet, filename, verb, noun,
                     elif pos.startswith('NN') and lemma_word in noun: pos_tag = 'N'
                     elif pos.startswith('J') and lemma_word in adj: pos_tag = 'ADJ'
                     
-                    if lemma_word in A_word:
-                        level = 'A'
-                    elif lemma_word in B_word:
-                        level = 'B'
+                    if lemma_word in A_word: level = 'A'
+                    elif lemma_word in B_word: level = 'B'
                     
                     if pos_tag:
-                        if lemma_word not in word_list:
-                            #file.write(lemma_word + '\t' + pos_tag + '\n')
-                            word_list.add(lemma_word)
-                            bf_tag = ""
-                            bb_tag = ""
-                        else:
-                            bf_tag = ""
-                            bb_tag = ""
-                            #pos_num += 1
-                            #continue
+                        word_list.add(lemma_word)
+                        bf_tag = bb_tag = ""
                         r = re.findall('(?P<f>\W)'+clean_word+'(?P<b>\W)', sent) # cookies
                         for f, b in r:
                             sent = sent.replace(f+clean_word+b, f+'<span data-word="'+lemma_word+'" data-level="'+level+'" data-pos="'+pos_tag+'">'+bf_tag+clean_word+bb_tag+'</span>'+b)
-                            if f == r[0][0]:
-                                _sent = _sent.replace(f+clean_word+b, f+'<span><u>'+bf_tag+clean_word+bb_tag+'</u></span>'+b)
                         if not r: # About ...
                             r = re.match(clean_word, sent)
                             sent = sent.replace(clean_word, '<span data-word="'+lemma_word+'" data-level="'+level+'" data-pos="'+pos_tag+'">'+bf_tag+clean_word+bb_tag+'</span>')
-                            _sent = _sent.replace(clean_word, '<span><u>'+bf_tag+clean_word+bb_tag+'</u></span>')
                     pos_num += 1
                 new_para += sent+' '
-                _tag_para += _sent+' '
-            c[1] = _tag_para
-            parts.append(Paragraph('<font size=14><b>[' + str(para_num) + ']</b></font> '+c[1], stylesheet['default']))
-            para_num += 1
             new_content.append(['p', new_para])
-            
-        elif _type == 'h2':
-            parts.append(Paragraph(text, stylesheet['h2']))
-            new_content.append(['h2', text])
-        elif _type == 'h3':
-            parts.append(Paragraph(text, stylesheet['h3']))
-            new_content.append(['h3', text])
-        
-    doc.build(parts)
-    #file.close()
+        elif _type == 'h2': new_content.append(['h2', text])
+        elif _type == 'h3': new_content.append(['h3', text])
+
     return new_content
