e213ed18a28890d47108651eb1d08287c4f87a77
PinYuan <c0919156850@gmail.com>
2018-07-12 21:59:21 +0800

    add wordPG dict

--- WEB/WEB/WEB.py
+++ WEB/WEB/WEB.py
@@ -5,62 +5,22 @@ from bs4 import BeautifulSoup
 import os
 import requests
 from collections import defaultdict
-
-from utils.extract import *
-from utils.voc_grading_and_detail import *
-from utils.create_pdf import *
-from utils.grammar_pattern import *
-from utils.autoFindPattern import *
-from readability import Document
-
-from utils.create_pdf.create_flashcard import *
-from utils.create_pdf.create_article import *
-from utils.create_pdf.create_wordlist import *
-from utils.create_pdf.create_grammar import *
-from utils.create_pdf.stylesheet import *
-
-from pprint import pprint
 import json
 
-# Read statistics file
-file = open('utils/data/autoFindPattern/statistics(V).txt', 'r')
-dictV =  defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: 0)))) 
-for line in file:
-    word, subDict = line.split('\t')
-    dictV[word] = eval(subDict)
-file.close()
+from utils.extract import *
+from utils.create_pdf import *
+from readability import Document
 
-file = open('utils/data/autoFindPattern/statistics(N).txt', 'r')
-dictN =  defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: 0)))) 
-for line in file:
-    word, subDict = line.split('\t')
-    dictN[word] = eval(subDict)
-file.close()
+from utils.create_pdf.create_article import *
 
-file = open('utils/data/autoFindPattern/statistics(Adj).txt', 'r')
-dictAdj =  defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: 0)))) 
-for line in file:
-    word, subDict = line.split('\t')
-    dictAdj[word] = eval(subDict)
-file.close()
+dictWord = eval(open('utils/data/autoFindPattern/wordPG.txt', 'r').read())
+phraseV = eval(open('utils/data/autoFindPattern/phrase(V).txt', 'r').read())
 
-
-# read statistic key
-file = open('utils/data/autoFindPattern/keys(V).txt', 'r')
-verb_set = eval(file.readline())
-file.close()
-
-file = open('utils/data/autoFindPattern/keys(N).txt', 'r')
-noun_set = eval(file.readline())
-file.close()
-
-file = open('utils/data/autoFindPattern/keys(Adj).txt', 'r')
-adj_set = eval(file.readline())
-file.close()
+# read translation
+TRANS = eval(open('utils/data/wordTranslation.txt', 'r').read()) # tran[pos][word] = [translation...]
 
 app = Flask(__name__ )
-
-stylesheet = stylesheet() # pdf stylesheet
+import datetime
 # egp = load_egp() # grammar pattern
 
 if not os.path.exists('download'):
@@ -74,25 +34,17 @@ def index():
 @app.route('/handle_data', methods=['POST', 'GET'])
 def handle_data():
     url = request.form['url']
-    #user_level = request.form['user_level']
+    user_level = request.form['user_level']
     response = requests.get(url)
-
     doc = Document(remove_a(response.text))
     title = doc.short_title()
     publish_date = getPublishDate(url)
     content = clean_content(doc.summary())
-    #grade, wordlist = voc_grading_and_detail(content, user_level)
-    #patterns = findGramPat(content)
-    # create pdf
-    #new = create_article(title, content, stylesheet, grade, 'download/'+title+'_article.pdf')
-    #print(content)
-    new = create_article(title, content, stylesheet,  'download/'+title+'_article.pdf', verb_set, noun_set, adj_set)
-    #create_wordlist(wordlist, patterns, 'download/'+title+'_wordlist.pdf')
-    # create_grammar(title, original, stylesheet, egp, 'download/'+title+'_grammar.pdf')
+    new = create_article(title, user_level, content,  'download/'+title+'_article.pdf', \
+                         set(dictWord['V'].keys()), set(dictWord['N'].keys()), set(dictWord['ADJ'].keys()))
     
-    
-    
-    return render_template('format.html', title=title, publish_date=publish_date, content=new) #, user_level=user_level , grade=grade
+    return render_template('format.html', title=title, publish_date=publish_date, \
+                           user_level=user_level, content=new) 
 
 @app.route('/download/<filename>', methods=['GET'])
 def return_reformatted(filename):
@@ -104,46 +56,36 @@ def return_reformatted(filename):
 @app.route('/ajax', methods = ['POST'])
 def ajax_request():
     word = request.form['word'].lower() if request.form['pos'] != 'x' else request.form['word'].split()[0].lower()  
-    pos = [request.form['pos']] if request.form['pos'] != 'x' else [p.upper() for p in request.form['word'].split()[1:]]
+    poses = ['V', 'N', 'ADJ'] if len(request.form['word'].split())==1 else [p.upper() for p in request.form['word'].split()[1:]]
     
-    targetList = []
-    targetDict = dict()
-    if 'V' in pos: targetList.append(dictV)
-    if 'N' in pos: targetList.append(dictN)
-    if 'ADJ' in pos: targetList.append(dictAdj)
-    if not targetList: targetList = [dictV, dictN, dictAdj]
+    # patternTable[pos] = [(pat, colls, (en, ch, source)), ...] 
+    patternTable = defaultdict(lambda: [])
+    # phraseTable[pos][phrase] = [pat, (colls, (en, ch, source)), ...] 
+    phraseTable = defaultdict(lambda: defaultdict(lambda: []))
+    # phraseOrder = [phrase...]
+    phraseOrder = []
+    # trans[pos] = [translation]
+    trans = defaultdict(lambda: defaultdict(lambda: set())) 
     
-    result = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: ''))) # result[pos][pat][obj] = highlight
-    patPair = defaultdict(lambda: []) # patPair[pos] = [pat...]
-    objs = defaultdict(lambda: defaultdict(lambda: [])) # objs[pos][pat] = [obj...]
-        
-    for targetDict in targetList:
-        if targetDict == dictV: mark = 'V'
-        elif targetDict == dictN: mark = 'N'
-        else: mark = 'ADJ'
-            
-        if word in targetDict.keys():
-            patPair[mark] = sorted(targetDict[word].keys(), key=lambda x: -(int(x.rsplit('%', 1)[1])))[:5]
-            subpatPair = patPair[mark]
-            
-            sub_objs = defaultdict(lambda: []) # objs[pat] = [obj...]
+    for pos in poses:
+        if word in dictWord[pos].keys():
+            # TODO須處理個數，以後可能動態
+            for pat, colls, examp in dictWord[pos][word][:5]:
+                patternTable[pos] += [(pat, ', '.join(colls[:3]), examp)]
 
-            for pat in subpatPair:
-                objPair = sorted(targetDict[word][pat].keys(), key=lambda x: -(int(x.rsplit('%', 1)[1])))[:3]
-                # move '-' to list end
-                emptys = [pair for pair in objPair if pair.startswith('-')]
-                for empty in emptys:
-                    objPair.remove(empty)
-                    objPair += [empty]
+        if pos == 'V' and word in phraseV.keys():
+            # 前面以過濾過phrase至多3個, pat已用std過濾
+            phraseOrder = sorted(phraseV[word].keys(), key=lambda x: -int(x.rsplit('%', 1)[1]))
+            for phrase in phraseOrder:
+                for pat, colls, examp in phraseV[word][phrase]:
+                    phraseTable[pos][phrase] += [(pat, ', '.join(colls[:3]), examp)]
 
-                sub_objs[pat] += objPair
-                for obj in objPair:
-                    highlight = sorted(targetDict[word][pat][obj].items(), key=lambda x: -x[1])[0]
-                    result[mark][pat][obj] = highlight[0]
-                    
-            objs[mark] = sub_objs
-                      
-    return jsonify(table=result, patterns=patPair, objs=objs)
+        if word in TRANS[pos].keys():
+            trans[pos] = TRANS[pos][word]
+                             
+    return jsonify(patternTable=patternTable, \
+                   phraseTable=phraseTable, phraseOrder=phraseOrder, \
+                   trans=trans)
 
 #static url cache buster
 @app.context_processor
@@ -160,5 +102,5 @@ def dated_url_for(endpoint, **values):
     return url_for(endpoint, **values)   
 
 if __name__ == '__main__':
-    #app.run(debug=False)
+#     app.run(debug=False)
     app.run(host='0.0.0.0', port=int("5487"), debug=False)
